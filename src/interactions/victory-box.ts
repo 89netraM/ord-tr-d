a20cf4e5abd92626b031c9fcbad589fe2a544ed6
Mårten Åsberg <marten.asberg@outlook.com>
2025-02-23 20:00:08 +0100

    Improve sharing

--- src/interactions/victory-box.ts
+++ src/interactions/victory-box.ts
@@ -3,7 +3,6 @@ import { WordTreeRenderer } from "../WordTreeRenderer";
 
 export const victoryBox = document.getElementById("victory-box") as HTMLVictoryBoxElement;
 const stepsDisplay = document.getElementById("steps") as HTMLSpanElement;
-const shareParagraph = document.getElementById("share-paragraph") as HTMLParagraphElement;
 const shareButton = document.getElementById("share-button") as HTMLButtonElement;
 
 const shareCanvas = document.getElementById("share-canvas") as HTMLCanvasElement;
@@ -42,25 +41,25 @@ victoryBox.showVictory = (startNode: WordNode, endNode: WordNode): void => {
     const steps = endNode.level - startNode.level;
     stepsDisplay.innerText = `${steps}`;
 
-    shareData = {
-        title: "Ord Tråd",
-        text: `Jag löste dagens Ord Tråd på ${steps} drag!`,
-        url: window.location.href,
-    };
-    shareParagraph.style.display = navigator.canShare(shareData) ? "block" : "none";
-
     // Render to canvas
     wordTreeRenderer.rootNode = startNode;
     wordTreeRenderer.wordTransformer = n => n.word;
     wordTreeRenderer.renderContained();
     shareCanvasCtx.transferFromImageBitmap(offscreenCanvas.transferToImageBitmap());
-    // Render to screenshot
-    wordTreeRenderer.wordTransformer = n =>
-        n === startNode || n.word === endNode.word
-            ? n.word
-            : [...n.word].map(_ => letters[Math.floor(Math.random() * letters.length)]).join("");
-    wordTreeRenderer.renderWordTree();
-    offscreenCanvas.convertToBlob().then(shareScreenshot);
+
+    if ("share" in navigator) {
+        shareData = {
+            text: `Jag löste dagens Ord Tråd på ${steps} drag!\nhttps://ord-tråd.se/`,
+        };
+        shareButton.style.display = navigator.canShare(shareData) ? "flex" : "none";
+        // Render to screenshot
+        wordTreeRenderer.wordTransformer = n =>
+            n === startNode || n.word === endNode.word
+                ? n.word
+                : [...n.word].map(_ => letters[Math.floor(Math.random() * letters.length)]).join("");
+        wordTreeRenderer.renderWordTree();
+        offscreenCanvas.convertToBlob().then(shareScreenshot);
+    }
 
     victoryBox.showModal();
 };
@@ -70,14 +69,14 @@ function shareScreenshot(blob: Blob | null): void {
         return;
     }
 
-    const screenshot = new File([blob], "ord-trad.png", { type: "image/png" });
+    const screenshot = new File([blob], "ord-trad.png", { type: blob.type });
     const shareDataWithScreenshot: ShareData = {
         ...shareData,
         files: [screenshot],
     };
     if (navigator.canShare(shareDataWithScreenshot)) {
         shareData = shareDataWithScreenshot;
-        shareParagraph.style.display = "block";
+        shareButton.style.display = "flex";
     }
 }
 
